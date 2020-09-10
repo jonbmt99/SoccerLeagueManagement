7b8838b51725e8441ebc0d43c8eabeac5a2156b4
Truong <mrherik@gmail.com>
2020-09-10 18:47:45 +0700

    setup-view

--- app/dao.py
+++ app/dao.py
@@ -7,5 +7,15 @@ from app.models import Team, Player
 def read_team():
     return Team.query.all()
 
-def read_player():
-    return Player.query.all()
+def read_player(keyword=None, position=None):
+    p = Player.query
+
+    if keyword:
+        p = p.filter(Player.name.contains(keyword))
+    if position:
+        p = p.filter(Player.position.contains(position))
+    return p.all()
+
+def read_team_by_id(id:int):
+    return Team.query.get(id)
+
