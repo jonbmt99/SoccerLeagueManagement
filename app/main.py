b301ce430cc33b68059ffd831221be0c61d33b88
DaiLocIU <62388836+DaiLocIU@users.noreply.github.com>
2020-09-12 16:01:54 +0700

    setup-login-cart
    setup-login-cart

--- app/main.py
+++ app/main.py
@@ -1,53 +1,151 @@
-from flask import render_template, request
+from flask import render_template, request, url_for, session, flash
 from flask_login import login_user
 from app import app, login, dao
+from app.forms import extractAndPersistUserDataFromForm, is_valid, getLoginUserDetails, getProductDetails, \
+    extractAndPersistKartDetailsUsingSubquery, isUserLoggedIn, getusercartdetails, removeProductFromCart, \
+    extractOrderdetails
 from app.models import *
 import hashlib
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    logged_in, username, productCountinCartForGivenUser = getLoginUserDetails()
+    return render_template(
+        "index.html",
+        loggedIn=logged_in,
+        username=username,
+        productCountinCartForGivenUser = productCountinCartForGivenUser
+    )
 
 @app.route("/team")
 def teams():
     return render_template("team.html", teamList=dao.read_team())
 
-# @app.route("/search-player", methods=['post', 'get'])
-# def SearchPlayer():
-#     data_form = request.form['position']
-#     print(data_form)
-#     return "ádsa"
-
-@app.route("/ticket")
-def tickets():
-    return render_template("ticket.html")
-
 @app.route("/player")
 def players():
-    kw = request.args.get("keyword")
-    position = request.args.get("position")
-    return render_template("player.html", playerList=dao.read_player(keyword=kw, position=position))
+    return render_template("player.html", playerList=dao.read_player())
 
-@app.route("/team-detail")
-def team_details():
-    team_id = request.args.get("teamId")
-    # team_test = dao.read_team_by_id(team_id)
-    # print(team_test)
-    return render_template("teamDetail.html", team=dao.read_team_by_id(team_id))
+@app.route("/payment")
+def payment():
+    return render_template("payment.html", resss=dao.payment_momo())
 
-@app.route("/matches")
-def matches():
-    return render_template("match.html")
+
+@app.route("/registerationForm")
+def registration_form():
+    return render_template("register.html")
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # Parse form data
+        msg = extractAndPersistUserDataFromForm(request)
+        return render_template("login.html", error=msg)
+
+@app.route("/login", methods=['POST', 'GET'])
+def user_login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        if is_valid(email, password):
+            session['email'] = email
+            return redirect('/')
+        else:
+            error = 'Invalid UserId / Password'
+            return render_template('login.html', error=error)
+
+@app.route("/logout")
+def logout():
+    session.pop('email', None)
+    return redirect(url_for('index'))
+
+
+@app.route("/signIn")
+def loginForm():
+    if 'email' in session:
+        return redirect(url_for('index'))
+    else:
+        return render_template('login.html', error='')
+
+
+@app.route("/displayProduct")
+def displayProduct():
+    product_list = Product.query.all()
+    return render_template('displayProduct.html', productList=product_list)
+
+
+
+@app.route("/productDescription")
+def productDescription():
+    productid = request.args.get('productId')
+    productDetailsByProductId = getProductDetails(productid)
+    return render_template("productDescription.html", data=productDetailsByProductId)
+
+
+@app.route("/addToCart")
+def addToCart():
+    if isUserLoggedIn():
+        productId = int(request.args.get('productId'))
+
+        # Using Flask-SQLAlchmy SubQuery
+        extractAndPersistKartDetailsUsingSubquery(productId)
+
+        # Using Flask-SQLAlchmy normal query
+        # extractAndPersistKartDetailsUsingkwargs(productId)
+        flash('Item successfully added to cart !!', 'success')
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('loginForm'))
+
+@app.route("/shopcart")
+def shopcart():
+    if isUserLoggedIn():
+        loggedIn, firstName, total_productincart = getLoginUserDetails()
+        productsincart, totalsum= getusercartdetails();
+        return render_template("shopcart.html", productsincart=productsincart , totalsum = totalsum)
+    else:
+        return redirect(url_for('index'))
+
+@app.route("/removeFromCart")
+def removeFromCart():
+    if isUserLoggedIn():
+        productId = int(request.args.get('productId'))
+        removeProductFromCart(productId)
+        return redirect(url_for('shopcart'))
+    else:
+        return redirect(url_for('loginForm'))
+
+
+@app.route("/checkoutPage")
+def checkoutForm():
+    if isUserLoggedIn():
+        productsincart, totalsum = getusercartdetails()
+        return render_template("checkoutPage.html", productsincart=productsincart, totalsum=totalsum)
+    else:
+        return redirect(url_for('loginForm'))
+
+
 
 @app.route("/login-admin", methods = ['GET', 'POST'])
 def login_admin():
     if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password", "")
+        email = request.form['email']
+        password = request.form['password']
         password = str(hashlib.md5(password.strip().encode("utf-8")).hexdigest())
-        user = User.query.filter(User.username == username.strip(), User.password == password).first()
+        user = User.query.filter(User.email == email, User.password == password, User.isAdmin).first()
         if user:
             login_user(user=user)
-    return redirect("/admin")
+            return redirect("/admin")
+
+
+@app.route("/createOrder", methods=['GET', 'POST'])
+def createOrder():
+    totalsum = request.args.get('total')
+    orderid = extractOrderdetails(totalsum)
+    print(orderid)
+    return render_template("OrderPage.html",orderid=orderid)
+
+
 
 
 @login.user_loader
--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean,  ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, DECIMAL
 from sqlalchemy.orm import relationship
 from flask_admin.contrib.sqla import ModelView
 from flask_admin import BaseView, expose
@@ -6,13 +6,15 @@ from flask_login import UserMixin, current_user, logout_user
 from flask import redirect
 from app import db, admin
 
+
 class User(db.Model, UserMixin):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False)
     active = Column(Boolean, default=True)
     username = Column(String(50), unique=True, nullable=False)
     password = Column(String(50), nullable=False)
+    isAdmin = Column(Boolean, default=False)
 
     def __str__(self):
         return self.name
@@ -46,6 +48,7 @@ class Player(db.Model):
     def __str__(self):
         return self.name
 
+
 class Match(db.Model):
     __tablename__ = "match"
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -58,6 +61,10 @@ class Match(db.Model):
     result = Column(String(10), nullable=False)
     round_id = Column(Integer, ForeignKey('round.id'), nullable=False)
 
+    def __str__(self):
+        return self.name
+
+
 class Score(db.Model):
     __tablename__ = "score"
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -69,17 +76,29 @@ class Score(db.Model):
     player = relationship("Player", foreign_keys=[player_id])
     type_of_score_id = Column(Integer, ForeignKey('typeofscore.id'), nullable=False)
 
+    def __str__(self):
+        return self.name
+
+
 class TypeOfScore(db.Model):
     __tablename__ = "typeofscore"
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(50), nullable=False)
     scores = relationship('Score', backref='Type of score', lazy=True)
 
+    def __str__(self):
+        return self.name
+
+
 class Round(db.Model):
     __tablename__ = "round"
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(Integer, nullable=False)
     matches = relationship('Match', backref='round', lazy=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Rule(db.Model):
     __tablename__ = "rule"
@@ -94,9 +113,56 @@ class Rule(db.Model):
     draw_point = Column(Integer, nullable=False)
     lose_point = Column(Integer, nullable=False)
 
+    def __str__(self):
+        return self.name
+
+
+class Product(db.Model):
+    __tablename__ = "product"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    product_name = Column(String(100), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    regular_price = Column(DECIMAL, nullable=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Cart(db.Model):
+    __tablename__ = "cart"
+    userid = Column(Integer, ForeignKey('user.id'), nullable=False, primary_key=True)
+    productid = Column(Integer, ForeignKey('product.id'), nullable=False, primary_key=True)
+    quantity = Column(Integer, nullable=False)
+
+    def __str__(self):
+        return self.name
+
+class Order(db.Model):
+        __tablename__ = "order"
+        id = Column(Integer, primary_key=True, autoincrement=True)
+        order_date = Column(DateTime, nullable=False)
+        total_price = Column(DECIMAL, nullable=False)
+        userid = db.Column(Integer, ForeignKey('user.id'), nullable=False, primary_key=True)
+
+        def __str__(self):
+            return self.name
+
+
+class OrderedProduct(db.Model):
+    __tablename__ = "order_product"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    orderid = Column(Integer, ForeignKey('order.id'), nullable=False)
+    productid = Column(Integer, ForeignKey('product.id'), nullable=False)
+    quantity = Column(Integer, nullable=False)
+
+    def __str__(self):
+        return self.name
+
+
 class AuthenticatedView(ModelView):
     def is_accessible(self):
         return current_user.is_authenticated
+
 
 class AboutUsView(BaseView):
     @expose("/")
@@ -105,6 +171,7 @@ class AboutUsView(BaseView):
 
     def is_accessible(self):
         return current_user.is_authenticated
+
 
 class LogOutView(BaseView):
     @expose('/')
@@ -115,6 +182,7 @@ class LogOutView(BaseView):
     def is_accessible(self):
         return current_user.is_authenticated
 
+
 admin.add_view(AuthenticatedView(Team, db.session))
 admin.add_view(AuthenticatedView(Player, db.session))
 admin.add_view(AuthenticatedView(Match, db.session))
@@ -122,9 +190,12 @@ admin.add_view(AuthenticatedView(Score, db.session))
 admin.add_view(AuthenticatedView(TypeOfScore, db.session))
 admin.add_view(AuthenticatedView(Round, db.session))
 admin.add_view(AuthenticatedView(Rule, db.session))
+admin.add_view(AuthenticatedView(Product, db.session))
+admin.add_view(AuthenticatedView(Cart, db.session))
+admin.add_view(AuthenticatedView(Order, db.session))
+admin.add_view(AuthenticatedView(OrderedProduct, db.session))
 admin.add_view(AboutUsView(name="About Us"))
 admin.add_view(LogOutView(name="Log out"))
 
 if __name__ == "__main__":
     db.create_all()
-
